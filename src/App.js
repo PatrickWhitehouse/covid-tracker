9c0e220171485ea23116812c70fbc022fc4b6f92
Patrick Whitehouse <patrick1996@gmail.com>
2020-07-05 16:21:35 +0100

    api call using axios async

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  useEffect(() => {
+    async function getData() {
+      const res = await Axios.get("https://covid19.mathdro.id/api");
+      console.log(res);
+    }
+    getData();
+  });
   return (
     <div className="App">
       <h1>Hello</h1>
