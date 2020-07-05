0a34567010d392fb3949eecbf1d52200fe61155b
Patrick Whitehouse <patrick1996@gmail.com>
2020-07-05 17:40:06 +0100

    fetch data

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 
-function Cards() {
-  return <h1>Cards</h1>;
+function Cards({ data }) {
+  if (!data) {
+    return "Fetching data...";
+  }
+  return (
+    <Grid container spacing={3} justify="center">
+      <Grid item component={Card}>
+        <CardContent>
+          <Typography color="textSecondary" gutterBottom>
+            Infected
+          </Typography>
+          <Typography variant="h6">Data</Typography>
+          <Typography variant="h6" color="textSecondary">
+            Date
+          </Typography>
+          <Typography variant="body2">Number of cases: </Typography>
+        </CardContent>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Cards;
