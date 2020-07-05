7a0ceccdb38e893d539a8cac5074f9acd97cb0b1
Patrick Whitehouse <patrick1996@gmail.com>
2020-07-05 16:27:03 +0100

    basic state updater

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React, { useState, useEffect } from "react";
 import Axios from "axios";
 
 function App() {
+  const [deaths, setDeaths] = useState(0);
+
   useEffect(() => {
     async function getData() {
       const res = await Axios.get("https://covid19.mathdro.id/api");
-      console.log(res);
+      setDeaths(res.data.confirmed.value);
     }
     getData();
   });
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Covid 19 tracker.</h1>
+      <h2>Current deaths are {deaths}</h2>
     </div>
   );
 }
