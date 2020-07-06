d5d33dffee471d1ce3fa2090f1f4e38aa87d9a2f
Patrick Whitehouse <patrick.whitehouse@pushon.co.uk>
2020-07-06 19:51:32 +0100

    change wording

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div>
       <h1 className="text-5xl">Covid 19 Stats</h1>
       <div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
-        <p>This tracker uses the data from the following API - https://disease.sh/v3/covid-19/. This means that the data used may not be 100% accurate.</p>
+        <p>This tracker uses the data from the following API - https://disease.sh/v3/covid-19/. This means that the data displayed may not be 100% accurate.</p>
       </div>
       <Global />
       <Continents />
